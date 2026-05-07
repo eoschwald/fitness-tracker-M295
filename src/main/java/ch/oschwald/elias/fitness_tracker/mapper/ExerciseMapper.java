bd3a21e29764d4697d4c6eff4d66fa387ffd46a6
Elias Oschwald <eoschwald@adobe.com>
2026-05-07 21:54:25 +0200

    feat: add dto mapper utilities

--- src/main/java/ch/oschwald/elias/fitness_tracker/mapper/ExerciseMapper.java
+++ src/main/java/ch/oschwald/elias/fitness_tracker/mapper/ExerciseMapper.java
@@ -1,4 +1,35 @@
 package ch.oschwald.elias.fitness_tracker.mapper;
 
+import ch.oschwald.elias.fitness_tracker.dto.ExerciseRequest;
+import ch.oschwald.elias.fitness_tracker.dto.ExerciseResponse;
+import ch.oschwald.elias.fitness_tracker.entity.Exercise;
+
 public class ExerciseMapper {
+
+    private ExerciseMapper() {
+    }
+
+    public static Exercise toEntity(ExerciseRequest request) {
+        Exercise exercise = new Exercise();
+        exercise.setName(request.getName());
+        exercise.setRepetitions(request.getRepetitions());
+        exercise.setDurationInMinutes(request.getDurationInMinutes());
+        exercise.setWeight(request.getWeight());
+        return exercise;
+    }
+
+    public static ExerciseResponse toResponse(Exercise exercise) {
+        ExerciseResponse response = new ExerciseResponse();
+        response.setId(exercise.getId());
+        response.setName(exercise.getName());
+        response.setRepetitions(exercise.getRepetitions());
+        response.setDurationInMinutes(exercise.getDurationInMinutes());
+        response.setWeight(exercise.getWeight());
+
+        if (exercise.getWorkout() != null) {
+            response.setWorkoutId(exercise.getWorkout().getId());
+        }
+
+        return response;
+    }
 }
--- src/main/java/ch/oschwald/elias/fitness_tracker/mapper/WorkoutMapper.java
+++ src/main/java/ch/oschwald/elias/fitness_tracker/mapper/WorkoutMapper.java
@@ -1,4 +1,57 @@
 package ch.oschwald.elias.fitness_tracker.mapper;
 
+import ch.oschwald.elias.fitness_tracker.dto.ExerciseResponse;
+import ch.oschwald.elias.fitness_tracker.dto.WorkoutRequest;
+import ch.oschwald.elias.fitness_tracker.dto.WorkoutResponse;
+import ch.oschwald.elias.fitness_tracker.entity.Exercise;
+import ch.oschwald.elias.fitness_tracker.entity.Workout;
+
+import java.util.List;
+
 public class WorkoutMapper {
+
+    private WorkoutMapper() {
+    }
+
+    public static Workout toEntity(WorkoutRequest request) {
+        Workout workout = new Workout();
+        workout.setTitle(request.getTitle());
+        workout.setDescription(request.getDescription());
+        workout.setWorkoutDate(request.getWorkoutDate());
+        return workout;
+    }
+
+    public static WorkoutResponse toResponse(Workout workout) {
+        WorkoutResponse response = new WorkoutResponse();
+        response.setId(workout.getId());
+        response.setTitle(workout.getTitle());
+        response.setDescription(workout.getDescription());
+        response.setWorkoutDate(workout.getWorkoutDate());
+
+        if (workout.getUser() != null) {
+            response.setUserId(workout.getUser().getId());
+        }
+
+        if (workout.getExercises() != null) {
+            response.setExercises(
+                    workout.getExercises().stream()
+                            .map(WorkoutMapper::mapExercise)
+                            .toList()
+            );
+        } else {
+            response.setExercises(List.of());
+        }
+
+        return response;
+    }
+
+    private static ExerciseResponse mapExercise(Exercise exercise) {
+        ExerciseResponse response = new ExerciseResponse();
+        response.setId(exercise.getId());
+        response.setName(exercise.getName());
+        response.setRepetitions(exercise.getRepetitions());
+        response.setDurationInMinutes(exercise.getDurationInMinutes());
+        response.setWeight(exercise.getWeight());
+        return response;
+    }
 }
