7389f97023328743ef0686b5bc927afa140d52c9
Elias Oschwald <eoschwald@adobe.com>
2026-05-07 15:21:40 +0200

    feat: add global exception handling

--- src/main/java/ch/oschwald/elias/fitness_tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/ch/oschwald/elias/fitness_tracker/exception/GlobalExceptionHandler.java
@@ -1,4 +1,43 @@
 package ch.oschwald.elias.fitness_tracker.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    //404 Fehler
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(NotFoundException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
+    }
+
+    //Validation Fehler (z. B. @NotBlank verletzt)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(err ->
+                errors.put(err.getField(), err.getDefaultMessage())
+        );
+
+        return errors;
+    }
+
+    //Allgemeine Fehler (Fallback)
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleGeneral(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Interner Serverfehler");
+        return error;
+    }
 }
--- src/main/java/ch/oschwald/elias/fitness_tracker/exception/NotFoundException.java
+++ src/main/java/ch/oschwald/elias/fitness_tracker/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package ch.oschwald.elias.fitness_tracker.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
