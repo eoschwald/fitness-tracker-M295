4e1c73c70b57ca5968a462a6eaaa12eb7941bd48
Elias Oschwald <eoschwald@adobe.com>
2026-05-08 08:00:54 +0200

    feat: configure openapi swagger documentation

--- src/main/java/ch/oschwald/elias/fitness_tracker/config/OpenApi30Config.java
+++ src/main/java/ch/oschwald/elias/fitness_tracker/config/OpenApi30Config.java
@@ -1,4 +1,37 @@
 package ch.oschwald.elias.fitness_tracker.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Fitness Tracker API",
+                version = "1.0",
+                description = "REST API für Workouts, Exercises und Users",
+                contact = @Contact(name = "Elias Oschwald")
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApi30Config {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Fitness Tracker API")
+                        .version("1.0")
+                        .description("REST API für Workouts, Exercises und Users"));
+    }
 }
